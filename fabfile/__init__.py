12cafd86a51fd934f03d51964711c65fe700dedb
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-09-03 12:04:19 +0100

    Bump jdk to 7u65

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -51,7 +51,7 @@ def internal_provision_puppet(environment):
     # Download Java
     print 'Downloading jdk6 on to the puppet machine. This will take some time and be silent. Hang in there.'
     with cd('/etc/puppet/puppet-hilary/modules/oracle-java/files'):
-        run('wget https://s3.amazonaws.com/oae-performance-files/jdk-6u45-linux-x64.bin')
+        run('wget https://s3.amazonaws.com/oae-performance-files/jdk-7u65-linux-x64.gz')
 
     # Place the common_hiera_secure if one is specified
     hiera_secure_path = '%s/common_hiera_secure.json' % environment
