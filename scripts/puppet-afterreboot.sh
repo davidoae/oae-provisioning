f4701e1005afb4b2fae7a0dedaf1c7083b6b9a7a
David Stringer <David.Stringer@researchresearch.com>
2016-12-05 12:02:21 +0000

    purple better, red indicates errs/warnings

--- scripts/puppet-afterreboot.sh
+++ scripts/puppet-afterreboot.sh
@@ -100,7 +100,7 @@ bin/pull.sh
 
 # an attempt to give the admin time to upload common_hiera_secure.json
 myiptemp="$(ip a show dev eth0 | awk '/inet / { print $2 }')"
-echo -e "\e[0;31mWaiting 30s to give time for admin to upload common_hiera_secure.json"
+echo -e "\e[0;35mWaiting 30s to give time for admin to upload common_hiera_secure.json"
 echo "Try this;"
 echo "  scp common_hiera_secure.json root@${myiptemp%%/*}:/etc/puppet/puppet-hilary/environments/${SCRIPT_ENVIRONMENT}/hiera/common_hiera_secure.json"
 echo -e "\e[0m"
